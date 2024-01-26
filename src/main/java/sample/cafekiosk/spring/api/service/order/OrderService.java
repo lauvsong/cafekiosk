d60798350291587bc7157f075cda80b962da84f6
김혜송(Hyesong Kim)/커머스플랫폼개발팀/SKP <hyesong.kim@sk.com>
2024-01-26 22:58:11 +0900

    feat: createOrder API completed

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -2,6 +2,8 @@ package sample.cafekiosk.spring.api.service.order;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
@@ -20,12 +22,21 @@ public class OrderService {
 
   public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
     List<String> productNumbers = request.getProductNumbers();
-    // Product
-    List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+    List<Product> duplicateProducts = findProductBy(productNumbers);
 
-    Order order = Order.create(products, registeredDateTime);
+    Order order = Order.create(duplicateProducts, registeredDateTime);
     Order savedOrder = orderRepository.save(order);
 
     return OrderResponse.of(savedOrder);
+  }
+
+  private List<Product> findProductBy(List<String> productNumbers) {
+    List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+    Map<String, Product> productMap = products.stream()
+        .collect(Collectors.toMap(Product::getProductNumber, product -> product));
+
+    return productNumbers.stream()
+        .map(productMap::get)
+        .toList();
   }
 }
