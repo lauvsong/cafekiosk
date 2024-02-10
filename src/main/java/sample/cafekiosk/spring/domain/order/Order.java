ecfa5a1b7b5b563e6ec5624eb57b87df20550d7e
김혜송(Hyesong Kim)/커머스플랫폼개발팀/SKP <hyesong.kim@sk.com>
2024-02-10 23:52:58 +0900

    feat: send order statistics mail

--- src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
@@ -39,20 +40,25 @@ public class Order extends BaseEntity {
   @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
   private List<OrderProduct> orderProducts = new ArrayList<>();
 
-  public Order(List<Product> products, LocalDateTime registeredDatetime) {
-    this.orderStatus = OrderStatus.INIT;
-    this.registeredDatetime = registeredDatetime;
+  @Builder
+  private Order(List<Product> products, OrderStatus orderStatus, LocalDateTime registeredDatetime) {
+    this.orderStatus = orderStatus;
     this.totalPrice = calculateTotalPrice(products);
+    this.registeredDatetime = registeredDatetime;
     this.orderProducts = products.stream()
         .map(product -> OrderProduct.create(this, product))
         .toList();
   }
 
-  private static int calculateTotalPrice(List<Product> products) {
-    return products.stream().mapToInt(Product::getPrice).sum();
+  public static Order create(List<Product> products, LocalDateTime registeredDatetime) {
+    return Order.builder()
+        .orderStatus(OrderStatus.INIT)
+        .products(products)
+        .registeredDatetime(registeredDatetime)
+        .build();
   }
 
-  public static Order create(List<Product> products, LocalDateTime registeredDatetime) {
-    return new Order(products, registeredDatetime);
+  private static int calculateTotalPrice(List<Product> products) {
+    return products.stream().mapToInt(Product::getPrice).sum();
   }
 }
